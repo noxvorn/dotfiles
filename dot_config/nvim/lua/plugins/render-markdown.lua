3cee1f483b0fc0599707499e433728efc1efa895
noxvorn <323563271+noxvorn@users.noreply.github.com>
2026-04-28 20:28:20 +0900

    chore: remove markdown render plugin

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		ft = { "markdown" },
-		opts = {},
-	},
-	{ "nvim-treesitter/nvim-treesitter", lazy = true },
-	{ "nvim-tree/nvim-web-devicons", lazy = true },
-}
